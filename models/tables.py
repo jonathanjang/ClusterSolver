44ee22eb52b44b86a0625eb1589a5823db1dea22
Jonathan Jang <jojjang@ucsc.edu>
2016-10-23 20:45:10 -0700

    form upload complete

---
+++ models/tables.py
@@ -0,0 +1,15 @@
+# Define your tables below (or better in another model file) for example
+#
+# >>> db.define_table('mytable', Field('myfield', 'string'))
+#
+# Fields can be 'string','text','password','integer','double','boolean'
+#       'date','time','datetime','blob','upload', 'reference TABLENAME'
+# There is an implicit 'id integer autoincrement' field
+# Consult manual for more options, validators, etc.
+
+import datetime
+
+db.define_table('import_data',
+				Field('file_name', requires=IS_NOT_EMPTY()),
+				Field('file_data', 'upload')
+                )
